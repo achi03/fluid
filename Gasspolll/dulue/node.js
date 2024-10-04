c48610da513a22a63e027b6be65fbd007cdfc341
achi03 <46640249+achi03@users.noreply.github.com>
2024-10-05 00:06:01 +0800

    Update node.js

--- Gasspolll/dulue/node.js
+++ Gasspolll/dulue/node.js
@@ -68,7 +68,7 @@
                 },
                 {
                 	"drm_id": "indoho",
-                   "origin": "https:\/\/idx.gdplayertv.to",
+                    "origin": "https:\/\/idx.gdplayertv.to",
                     "logo_url": "https:\/\/blogger.googleusercontent.com\/img\/b\/R29vZ2xl\/AVvXsEh3a59j4bHn_fH1Jt5LmmxUPrRgiqfSCOQzrDbbl6lpBlP5SWDOY5ozNT7lLeRReq3rN3oP67Eh28MnUhm95AQ4kA0pvPyRIyfpx6FJAXfYhq2KpBgHfiLgcaAp10cJidz9ej7-gUTDjfbX8gUZWRtVJpY-Hqjyw5kXZgi4zKygQY-X8TLlWgBfRvh5dmA\/s1280\/20240909_231650.jpg",
                     "name": "Citra Drama",
                     "stream_url": "https:\/\/biznet2.freemiumiptv.top/indihome130\/cdrama\/index.m3u8",
